feee8a17cf224ad75742227c0230edc1b49d2c04
Kaushal Raj Mishra <kaushu42@gmail.com>
2018-06-02 12:48:01 +0545

    Added character set for nepali characters

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from characters import characters
+print("***Nepali OCR***")
+verbose = True
 
 np.random.seed(1)
 
 def load_data():
+    if verbose:
+        print('Loading dataset...')
     m = 92000
     data = np.load('dataset/dataset.npz')
 
@@ -18,19 +23,25 @@ def load_data():
     return X, Y
 
 def shuffle(X, Y):
+    if verbose:
+        print('Shuffling data...')
     from sklearn.utils import shuffle
     X, Y = shuffle(X, Y)
     return X, Y
 
-def plot(X, Y, n):
+def plot(X, Y, n = 0):
+    print('The image is character: ', characters[Y[n][0] - 1])
     plt.imshow(X[n, :].reshape(32, 32), cmap = 'Greys')
     plt.show()
-    print(Y[n])
 
 def scale(X, factor = 255):
+    if verbose:
+        print('Scaling Features...')
     return X*(1/255)
 
-def split(x, y, ratio):
+def split(x, y, ratio = 0.2):
+    if verbose:
+        print('Splitting dataset...')
     from sklearn.model_selection import train_test_split
     return train_test_split(x, y, test_size = ratio)
 
@@ -38,6 +49,14 @@ def main():
     X, Y = load_data()
     X, Y = shuffle(X, Y)
     X = scale(X)
+    x_train, x_test, y_train, y_test = split(X, Y)
+
+    m_train = y_train.shape[0]
+    m_test =y_test.shape[0]
+
+    print('\nTraining Set size: ', m_train)
+    print('Test Set size: ', m_test)
+
 
 if __name__ == '__main__':
     main()
